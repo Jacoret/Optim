4fc636ed8464fb33b58cf09c4f9cffaa337de798
hippolytej <hippolyte.jacomet@gmail.com>
2017-02-16 11:11:39 +0100

    comments

--- part1.jl
+++ part1.jl
@@ -50,9 +50,20 @@ println("u1min : $u1min, f1min : $f1min")
 u2min, f2min = gradient_rho_adaptatif(f2, df2!, U0, 1e-4)
 println("u2min : $u2min, f2min : $f2min")
 
+# Question 2 bis -----------------------------------------
 @time optimize(f1, df1!, U0, BFGS())
-@time optimize(f1, df1!, U0, BFGS())
+@time resBFGS = optimize(f1, df1!, U0, BFGS())
 @time optimize(f1, df1!, U0, ConjugateGradient())
-@time optimize(f1, df1!, U0, ConjugateGradient())
+@time resCG = optimize(f1, df1!, U0, ConjugateGradient())
 @time optimize(f1, df1!, U0, GradientDescent())
-@time optimize(f1, df1!, U0, GradientDescent())
+@time resGD =optimize(f1, df1!, U0, GradientDescent())
+
+# Les algorithmes BFGS et Conjugate gradient, la descente de gradient est ~100 fois plus lente,
+# ce qui ne parait incohérent.
+
+println("BFGS : $resBFGS")
+println("Conjugate Gradient : $resCG")
+println("Gradient Descent : $resGD")
+
+# Question 3 --------------------------------------------
+# Avec un algorithme de gradient stochastique ?
